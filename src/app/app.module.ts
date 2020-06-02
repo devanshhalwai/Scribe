2a096116de8d9294775caaf55428eba26d546ff3
devanshhalwai <devansh.halwai@gmail.com>
2020-06-02 18:16:50 +0530

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import { LoginComponent } from './login/login.component';
+import { firebaseConfig } from './firebaseConfig.js';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBbQdBhS0bb3f3XwOFU0VBVNV1zFyGazrA",
-  authDomain: "scribe-devansh.firebaseapp.com",
-  databaseURL: "https://scribe-devansh.firebaseio.com",
-  projectId: "scribe-devansh",
-  storageBucket: "scribe-devansh.appspot.com",
-  messagingSenderId: "118432042776",
-  appId: "1:118432042776:web:38d64d2014205d914e186d",
-  measurementId: "G-VF36SDGD78"
-};
 
 firebase.initializeApp(firebaseConfig);
 
